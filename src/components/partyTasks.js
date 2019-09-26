e3c9d7cf7a21996f569bfbdd59fc151cdd954785
dang lu <danglu@dangs-mbp.lan>
2019-09-25 21:16:13 -0700

    got functionality up, added buttons to get back to parties

--- src/components/partyTasks.js
+++ src/components/partyTasks.js
@@ -1,64 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Item } from 'semantic-ui-react';
-import { withFormik, Form, Field } from 'formik';
-import * as yup from 'yup';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { withFormik, Form, Field } from 'formik'
+import { Item } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import * as yup from 'yup'
+import axios from 'axios'
 
+const Tasks = ({ errors, touched, status }) => {
+  const [tasks, setTasks] = useState([])
 
-function Tasks({status}){
-	const [tasks, setTasks] = useState([])
-
-	useEffect(() => {
-    	if (status){
-    		setTasks([...tasks, status])}
+  useEffect(() => {
+    if (status){
+    setTasks([...tasks, status])}
   }, [status])
-	// useEffect to post each item into the DB 
 
-	return(
-		<div className="task">
-			<h1> To Do Lit for XXXX party </h1>
-			<div className="task-container">
-				<Form >
-					<Field type='text' name='task' placeholder='Task' /><br/>
-					<Field type='number' name='cost' placeholder='$'/><br/>
-					<button> Add </button>
-				</Form>
-				<ul>
-					{tasks.map((item, index)=>{
-						return (
-							<li key={index}> {item} </li>
-							)
-					})}
-				</ul>
-			</div>
-		</div>
-		)
+  return (
+  	<div className='create-box'>
+  		<h1> To-Do List for XXXXXX Party </h1>
+	    <Form >
+		      { touched.task && errors.task && <p className="error">{errors.task}</p> }
+		      <Field type="text" name="task" placeholder="Task"  /> 
+
+		      { touched.cost && errors.cost && <p className="error">{errors.cost}</p> }
+		      <Field type="number" name="cost" placeholder="Cost: $"  />
+
+		      { touched.category && errors.category && <p className="error">{errors.category}</p> }
+		      <Field component="select" name="category">
+		        <option value="" disabled> Select Category: </option>
+		        <option value="food"> Food </option>
+		        <option value="drinks"> Drinks </option>
+		        <option value="entertainment"> Entertainment </option>
+		      </Field>
+
+		      <Field component="textarea" name="notes" placeholder="Notes" />
+
+		      <button type="submit">Submit</button>
+		      <Link to='/parties'> Back to Parties </Link>
+	    </Form>
+
+	    <div className='task-list'>
+
+	      {tasks.map((item, index) => (
+	        <Item key={index}> 
+	        Item: {item.task} <br/>
+	        Cost: {item.cost}
+	        </Item>
+	        
+	        ))}
+      	</div>
+
+
+    </div>
+  )
 }
 
 export default withFormik({
-	mapPropsToValues: (values) => {
-		return {
-			task: values.task || '',
-			cost: values.cost || '',
-		}
-	},
-	validationSchema : yup.object().shape({
-		task: yup
-			.string()
-			.required('Please enter in a task'),
-		cost: yup
-			.number()
-			.required('Please enter the cost'),
-	}),
-	handleSubmit: (values, { setStatus }) => {
-
-
-		axios.post('https://reqres.in/api/users', values)
-			.then((resp)=>{
-				setStatus(resp)
-				console.log(resp)
-			})
-			.catch((err)=> console.log(err))
-	}
-})(Tasks);
+  mapPropsToValues: (values) => {
+    return {
+      task: values.task || '',
+      cost: values.cost || '',
+      category: values.category || '',
+      notes: values.notes || ''
+    }
+  },
+  validationSchema: yup.object().shape({
+    task: yup.string().required('task is required'),
+    cost: yup.number().positive().required('cost is required'),
+    category: yup.string().required('category is required'),
+  }),
+  handleSubmit: (values, { setStatus }) => {
+    axios.post('https://reqres.in/api/tasks', values)
+      .then((resp) => {
+        setStatus(resp.data)
+      })
+      .catch(err => console.log(err))
+  }
+})(Tasks)
